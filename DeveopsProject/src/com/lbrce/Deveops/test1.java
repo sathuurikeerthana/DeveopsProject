aea603fc14ceb54469eb07e0ba73bde9ee57c5fb
AdmiN <AdmiN@ITWS09>
2023-08-10 14:57:40 +0530

    create new deveops project

---
+++ DeveopsProject/src/com/lbrce/Deveops/test1.java
@@ -0,0 +1,11 @@
+package com.lbrce.Deveops;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                 System.out.println("welcome");
+	}
+
+}
+ 
