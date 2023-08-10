e5c941e34c48c12611e33a2aaaf62026169fd39f
AdmiN <AdmiN@ITWS09>
2023-08-10 15:29:12 +0530

    test2 class

---
+++ DeveopsProject/src/com/lbrce/Deveops/test2.java
@@ -0,0 +1,11 @@
+package com.lbrce.Deveops;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.print("test2class");
+	}
+
+}
+ 
